2e95e47da179474077ed0f6110c59dddf801927e
Edward <jaysongraphics@gmail.com>
2021-05-17 20:18:13 -0400

    commit tree 93231eaeef9d0fd2c9272fae0493dce940ff997a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+let companyName = 'Scuber'; 
+
+function name(params) {
+    const companyName = 'Scuber';
+}
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
